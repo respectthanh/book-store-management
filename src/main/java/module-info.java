95b78b463397cc4240ba1515b356f6bd12596d19
respectthanh <respectthanh@gmail.com>
2024-12-22 23:47:08 +0700

    refactor project

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module capybara.bookstoremanagement {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens capybara.bookstoremanagement to javafx.fxml;
-    exports capybara.bookstoremanagement;
-}
